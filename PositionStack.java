874b8d9c76c82c7f16c1e966b5607c756ab24cd1
Rameen Ejaz <rameenejaz4@gmail.com>
2025-10-02 19:39:37 +0500

    making stack methods

--- PositionStack.java
+++ PositionStack.java
@@ -5,5 +5,22 @@ public class PositionStack {
         stack=new Position[size];
         top=-1;
     }
-    
+    void push (Position p) {
+        stack[++top]=p;
+    }
+    Position pop() {
+        return stack[top--];
+    }
+    Position peek() {
+        return stack[top];
+    }
+    boolean isEmpty() {
+        return top==-1;
+    }
+    void printPath() {
+        for (int i=0; i<=top; i++) {
+            System.out.println(stack[i] + " ");
+        }
+        System.out.println();
+    }
 }
