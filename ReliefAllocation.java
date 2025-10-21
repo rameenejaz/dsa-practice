c1d0c0faf636b5c328c3b5a4eb6edd97eb533097
Rameen Ejaz <rameenejaz4@gmail.com>
2025-10-21 12:42:55 +0500

    making a disaster relief system using priority queue

--- ReliefAllocation.java
+++ ReliefAllocation.java
@@ -1,15 +1,16 @@
 public class ReliefAllocation {
-    private String regionID;
-    private double urgencyLevel;
-    private double population;
-    private double accessIndex;
-    public ReliefAllocation(String regionID, double urgencyLevel, double population, double accessIndex) {
+    
+    protected String regionID;
+    protected int urgencyLevel;
+    protected int population;
+    protected int accessIndex;
+    public ReliefAllocation(String regionID, int urgencyLevel, int population, int accessIndex) {
         this.regionID=regionID;
         this.urgencyLevel=urgencyLevel;
         this.population=population;
         this.accessIndex=accessIndex;
     }
-    public double calcPriority(double urgencyLevel, double population, double accessIndex) {
+    public double calcPriority(int urgencyLevel, int population, int accessIndex) {
         double priority=0.6*urgencyLevel+0.3*(population/1000) + 0.1*(100-accessIndex);
         return priority;
     }
--- priorityQueue2.java
+++ priorityQueue2.java
@@ -4,5 +4,53 @@ public class priorityQueue2 {
     priorityQueue2() {
         region=new ArrayList<>();
     }
+    public void addRegion(String regionID, int urgencyLevel, int population, int accessIndex) {
+        region.add(new ReliefAllocation(regionID, urgencyLevel, population, accessIndex));
+    }
+    
+    public ReliefAllocation treatNextRegion() {
+        if (region.isEmpty()) {
+            System.out.println("No region to help");
+            return null;
+        }
+        int maxIndex=0;
+        for (int i=1; i<region.size(); i++) {
+            if(region.get(i).calcPriority(region.get(i).urgencyLevel,region.get(i).population, region.get(i).accessIndex)>region.get(maxIndex).calcPriority(region.get(maxIndex).urgencyLevel, region.get(maxIndex).population, region.get(maxIndex).accessIndex)) {
+                maxIndex=i;
+            }
+        }
+        return region.remove(maxIndex);
+    }
+    public void printQueue() {
+        if (region.isEmpty()) {
+            System.out.println("No regions to treat");
+            return;
+        }
+        else {
+            for (ReliefAllocation r: region) {
+                System.out.println("Region " + r.regionID + ", Urgency Level: " + r.urgencyLevel + " Population: " + r.population + ", Accesibility Index: " + r.accessIndex );
+
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        priorityQueue2 region=new priorityQueue2();
+        region.addRegion("R1", 90, 5000, 70);
+        region.addRegion("R2", 70, 8000, 60);
+        region.addRegion("R3", 85, 4000, 40);
+        region.addRegion("R4", 50, 10000, 90);
+        while(true) {
+            ReliefAllocation next=region.treatNextRegion();
+            if (next!=null) {
+                double priority=next.calcPriority(next.urgencyLevel, next.population, next.accessIndex);
+                System.out.println("Treating region: " + next.regionID + " (Priority " + priority + ")");
+            }
+            else {
+                break;
+            }
+
+        }
+    }
 
 }
