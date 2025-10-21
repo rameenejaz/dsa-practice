de4415c9154e43cbc34dbb1b0b23b9e0f0c7609e
Rameen Ejaz <rameenejaz4@gmail.com>
2025-10-21 14:39:32 +0500

    making a printer job scheduler

--- PrinterScheduler.java
+++ PrinterScheduler.java
@@ -1,12 +1,11 @@
 public class PrinterScheduler {
-    private String jobID;
-    private int priority;
-    private int pages;
-    private int submissionOrder;
+    private String jobID; 
+    private int priority; 
+    private int pages; 
+    private int submissionOrder; 
     PrinterScheduler(String jobID, int priority, int pages) {
-        this.jobID=jobID;
-        this.priority=priority;
-        this.pages=pages;
+        this.jobID=jobID; 
+        this.priority=priority; 
+        this.pages=pages; 
     }
-    
 }
