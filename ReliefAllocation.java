e2412e2f60a1bd8bc4d940610f702ebb14b957ef
Rameen Ejaz <rameenejaz4@gmail.com>
2025-10-21 12:05:30 +0500

    making a relief allocation program using priority queue

--- ReliefAllocation.java
+++ ReliefAllocation.java
@@ -1,13 +1,17 @@
 public class ReliefAllocation {
     private String regionID;
-    private int urgencyLevel;
-    private int population;
-    private int accessIndex;
-    public ReliefAllocation(String regionID, int urgencyLevel, int population, int accessIndex) {
+    private double urgencyLevel;
+    private double population;
+    private double accessIndex;
+    public ReliefAllocation(String regionID, double urgencyLevel, double population, double accessIndex) {
         this.regionID=regionID;
         this.urgencyLevel=urgencyLevel;
         this.population=population;
         this.accessIndex=accessIndex;
     }
-    
+    public double calcPriority(double urgencyLevel, double population, double accessIndex) {
+        double priority=0.6*urgencyLevel+0.3*(population/1000) + 0.1*(100-accessIndex);
+        return priority;
+    }
+
 }
