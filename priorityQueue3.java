d518d2cce2ae841332f5bd448462eb245b5e43b1
Rameen Ejaz <rameenejaz4@gmail.com>
2025-10-21 14:43:49 +0500

    making a printer job scheduler

---
+++ priorityQueue3.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class priorityQueue3 {
+    private ArrayList <PrinterScheduler> printer;
+    priorityQueue3() {
+        printer=new ArrayList<>();
+    }
+    
+}
