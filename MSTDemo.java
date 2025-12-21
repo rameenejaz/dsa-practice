b4376f1e3c79add6b0e691778d4c8fac1538b50c
Rameen Ejaz <rameenejaz4@gmail.com>
2025-12-21 23:56:22 +0500

    functions on minimum spanning tree using prim and kruskal algos

--- MSTDemo.java
+++ MSTDemo.java
@@ -1,13 +1,14 @@
+//functions on Minimum Spanning Tree (MST) using Prim's and Kruskal's algorithms
 import java.util.Scanner;
 
-public class MSTDemo {
+public class MSTDemo {  
 
     // A class to represent an edge.
     static class Edge {
         int src, dest, weight;
     }
 
-    
+
     // Disjoint Set Union (Union-Find) for Kruskal
     static class UnionFind {
         int[] parent;
